410c671c7232b6b7867b49f599dbc07e5543e338
Marcin Chodkowski <marcinshaz4m@gmail.com>
2025-11-27 16:13:54 +0100

    removed image feature

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -2,7 +2,6 @@ import {Body, Controller, Post} from '@nestjs/common';
 import {OpenaiService} from "./openai.service";
 import {SingleDishInputDto} from "./dtos/single-dish-input.dto";
 import {SingleDishOutputDto} from "./dtos/single-dish-output.dto";
-import {ImageDto} from "./dtos/image.dto";
 import {MealPlanInputDto} from "./dtos/meal-plan-input.dto";
 import {MealPlanOutputDto} from "./dtos/meal-plan-output.dto";
 
@@ -14,11 +13,6 @@ export class OpenaiController {
     @Post('single-dish-default')
     async getSingleDishDefault(@Body() singleDishInput: SingleDishInputDto): Promise<SingleDishOutputDto> {
         return await this.openaiService.generateSingleDishDefault(singleDishInput);
-    }
-
-    @Post('single-dish-image')
-    async getSingleDishImage(@Body() singleDishInput: SingleDishOutputDto): Promise<ImageDto> {
-        return this.openaiService.generateDishImage(singleDishInput);
     }
 
     @Post('meal-plan-default')
